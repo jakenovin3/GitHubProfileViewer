9af633349ec55f0190b8dfa4f300dccb67f95481
Jake Novin <jakenovin@gmail.com>
2026-05-21 20:45:19 -0700

    Added styling to the search bar. Added skeleton for repository section.

--- GitHubProfileViewer/src/App.jsx
+++ GitHubProfileViewer/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 
+
 // Components
 function SearchBar({onSearch}) {
   return(
     <div className="searchDiv">
-      <input type="searchField" id="profileSearch" name="profileSearch" placeholder="Search GitHub users..."></input>
+      <input type="searchField" id="profileSearch" name="profileSearch" placeholder="Enter GitHub username"></input>
       <button id="searchSubmit" onClick={onSearch}>Search</button>
     </div>
   );
@@ -12,7 +13,6 @@ function SearchBar({onSearch}) {
 
 function ProfileHeader({user}) {
   return(
-
     <div className="profileHeader">
       <div id="userInfo">
         <img id="profileImage" src={user?.avatar_url} alt="profile image"></img>
@@ -24,18 +24,29 @@ function ProfileHeader({user}) {
         <p><b>{user?.following}</b> following</p>
       </div>
     </div>
+  );
+}
+
+function RepoSection({repos}) {
+
+  
+
+  return(
+
+    <div className="userRepositories">
+      <Repository repoNumber={{}}/>
+    </div>
 
   );
 }
 
-function InfoCard({title, desc}) {
+function Repository({repoNumber}) {
+
+
+
   return(
-
-    <div className="infoCard">
-      <h4>{title}</h4>
-      <h6>{desc}</h6>
-    </div>
-
+    <>
+    </>
   );
 }
 
@@ -43,18 +54,29 @@ function InfoCard({title, desc}) {
 export default function App() {
 
   const [userData, setUserData] = useState(null);
+  const [repoData, setRepoData] = useState(null);
+
+  async function githubFetch(endpoint) {
+    const response = await fetch("https://api.github.com/" + endpoint);
+    if(!response.ok) { throw new Error("Response status: " + response.status); }
+    const data = await response.json();
+
+    return data;
+  }
 
   async function getUserData() {
-    // const searchedUsername = document.querySelector('#profileSearch').value;
-    const searchedUsername = "octocat";
+    
+    const searchedUsername = "octocat"; // document.querySelector('#profileSearch').value;
+
     try {
-      const response = await fetch("https://api.github.com/users/" + searchedUsername);
+      const [userData, repoData] = await Promise.all([
+        githubFetch("users/" + searchedUsername),
+        githubFetch("users/" + searchedUsername + "/repos")
+      ]);
 
-      if(!response.ok) { throw new Error("Response status: " + response.status); }
+      setUserData(userData);
+      setRepoData(repoData)
 
-      const data = await response.json();
-      setUserData(data);
-      console.log(data);
     } catch(error) {
       console.error("Here is the error: " + error);
     }
@@ -69,11 +91,9 @@ export default function App() {
     </div>
     <div className="main">
       <ProfileHeader user={userData} />
-      <div className="infoCardSection">
-        <InfoCard title={"Title 1"} desc={"Description 1 Description 1 Description 1"} />
-        <InfoCard title={"Title 2"} desc={"Description 2 Description 2 Description 2"} />
-        <InfoCard title={"Title 3"} desc={"Description 3 Description 3 Description 3"} />
-       </div>
+      <div className="profileBody">
+        <RepoSection user={repoData}/>
+      </div>
     </div>
     </>
   );
